33c2d5da9ef216f683dd0442fd91e04910108589
Imer Gonzalez <uchihaimerak1@gmail.com>
2023-04-25 13:51:55 -0600

    Test

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -11,7 +11,7 @@
         <div class="right-card">
             <div class="card-text">
                 <p>Hola,</p>
-                Soy Antonio. Full Stack Developer
+                Soy Imer. Full Stack Developer
             </div>
 
             <div class="card-icons">
